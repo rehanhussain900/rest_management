d8b5b359f74d039ef001890ba303ca74b680e0a5
rehanhussain900 <rehan@wanologicalsolutions.com>
2021-10-11 11:11:26 -0700

    This is resturent repositry synch to github

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,54 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<img alt="Delicious Food's" src="./assets/rest.jpg" class="rest_logo">
+<registration/>
 </template>
-
+     
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import registration from './components/registration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    registration
   }
-}
+}   
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.rest_logo {
+  width:100px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 30px;
+}
+.rest_form {
+  width:100px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+.register_form input{
+    display: block;
+    width: 300px;
+    height: 40px;
+    padding-left: 20px;
+    margin-left :auto;
+    margin-bottom: 30px;
+    margin-right: auto;
+    border: 1px solid skyblue;
+}
+.register_button{
+    width: 320px;
+    height: 40px;
+    border: 1px solid skyblue;
+    background : skyblue;
+    color : #fff;
+    cursor: pointer;
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    
 }
 </style>
---
+++ src/components/registration.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="rest_form">
+    <h1 class="">Sign Up </h1></div>
+    <div class="register_form">
+        
+        <input type="text" v-model="name"  placeholder="Enter Name" />
+        <input type="text" v-model="email" placeholder="Enter Email" />
+        <input type="text" v-model="password" placeholder="Enter Password" />
+        <button class="register_button" v-on:click="register">Sign Up</button>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+    name : 'registration',
+    data(){
+        return {
+            name :'',
+            email :'',
+            password:''
+        }
+    },
+    methods:{
+      async  register(){
+            
+            let result = await axios.post("http://localhost:3000/users",{
+                name:this.name,
+                email:this.email,
+                password:this.password
+            });
+            console.log(result);
+            if(result.status =="201"){
+                alert("Sign Up Successfully")
+            }else{
+                alert("Something Went Wrong")
+            }
+        }
+    }
+}
+
+</script>
